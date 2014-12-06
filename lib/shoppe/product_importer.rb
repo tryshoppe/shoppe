64a32600cde5b3b6348c897d5a7c831b217e8b8b
Luiz Varela <luiz.lvarela@gmail.com>
2014-12-06 20:31:20 -0300

    Create impoter class to products

--- lib/shoppe/product_importer.rb
+++ lib/shoppe/product_importer.rb
@@ -2,9 +2,8 @@ require 'roo'
 
 module Shoppe
   class ProductImporter
-    def initialize(file_path)
-      @file_path = file_path
-      @spreadsheet = open_spreadsheet(file_path)
+    def initialize(file)
+      @spreadsheet = open_spreadsheet(file)
     end
 
     def import!
@@ -12,18 +11,96 @@ module Shoppe
       header = @spreadsheet.row(1)
 
       (2..@spreadsheet.last_row).each do |i|
-        row = Hash[[header, spreadsheet.row(i)].transpose]
+        row = Hash[[header, @spreadsheet.row(i)].transpose]
+
+        row     = Row.new(row)
+        product = Shoppe::Product.find_by(name: row.name)
+
+        unless row.name.nil?
+          if product
+            if row.quantity > 0 && row.quantity != product.stock
+              product.stock_level_adjustments
+                .create!(description: I18n.t('shoppe.import'), adjustment: row.quantity)
+            end
+          else
+            product = create_product(row)
+            product.stock_level_adjustments
+              .create!(description: I18n.t('shoppe.import'),
+                       adjustment: row.quantity) if row.quantity > 0
+          end
+        end
+      end
+    end
+
+    def create_product(row)
+      product = Shoppe::Product.new(
+        name: row.name,
+        sku: row.sku,
+        description: row.description,
+        short_description: row.short_description,
+        weight: row.weight,
+        price: row.price,
+        product_category_id: product_category(row)
+      )
+      product.save!
+      product
+    end
+
+    def product_category(row)
+      category = Shoppe::ProductCategory.find_by(name: row.category_name)
+      if category
+        category.id
+      else
+        Shoppe::ProductCategory.create(name: row.category_name).id
       end
     end
 
     private
 
     def open_spreadsheet(file)
-      case file.extname(file.original_filename)
-      when '.csv' then roo::csv.new(file.path)
-      when '.xls' then roo::excel.new(file.path)
-      when '.xlsx' then roo::excelx.new(file.path)
-      else raise i18n.t('shoppe.imports.errors.unknown_format', filename: file.original_filename)
+      case File.extname(file.original_filename)
+      when '.csv' then Roo::CSV.new(file.path)
+      when '.xls' then Roo::Excel.new(file.path)
+      when '.xls' then Roo::Excelx.new(file.path)
+      else raise I18n.t('shoppe.imports.errors.unknown_format', filename: File.original_filename)
+      end
+    end
+
+    class Row
+      def initialize(row)
+        @row = row
+      end
+
+      def name
+        @row['name']
+      end
+
+      def sku
+        @row['sku']
+      end
+
+      def description
+        @row['description']
+      end
+
+      def short_description
+        @row['short_description']
+      end
+
+      def weight
+        @row['weight']
+      end
+
+      def price
+        @row['price'].nil? ? 0 : @row['price']
+      end
+
+      def quantity
+        @row['qty'].to_i
+      end
+
+      def category_name
+        @row['category_name']
       end
     end
   end
