7a5f4ea61ffa19306536ac2a9448e6455d030334
Aswani Nerella <aswani@getblueshift.com>
2016-08-22 01:32:55 -0700

    fix typo

--- app/models/shoppe/product.rb
+++ app/models/shoppe/product.rb
@@ -198,7 +198,7 @@ module Shoppe
 
     def self.open_spreadsheet(file)
       case File.extname(file.original_filename)
-      when '.csv' then Roo::CSV.new(file.path, csv_options: {col_sep: "|"}))
+      when '.csv' then Roo::CSV.new(file.path, csv_options: {col_sep: "|"})
       when '.xls' then Roo::Excel.new(file.path)
       when '.xlsx' then Roo::Excelx.new(file.path)
       else fail I18n.t('shoppe.imports.errors.unknown_format', filename: File.original_filename)
