3c83d3fc15d4c63b79bfe0e4dadf62c40ef9dae6
Joao Neto <jpsneto1@gmail.com>
2017-01-16 14:34:11 -0300

    Add restriction unique email_address to Shoppe users and prompt to filter category

--- app/models/shoppe/user.rb
+++ app/models/shoppe/user.rb
@@ -7,7 +7,7 @@ module Shoppe
     # Validations
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email_address, presence: true
+    validates :email_address, presence: true, uniqueness: true
 
     # The user's first name & last name concatenated
     #
